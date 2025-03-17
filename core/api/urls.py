7586c61bbda8154db7b37c192a77e987a87846d9
Amtilan <149930972+Amtilan@users.noreply.github.com>
2025-03-17 14:54:57 +0500

    add scalar viewer

--- core/api/urls.py
+++ core/api/urls.py
@@ -4,13 +4,18 @@ from ninja import NinjaAPI
 
 from core.api.schemas import PingResponseSchema
 from core.api.v1.urls import router as v1_router
+from scalar_django_ninja import ScalarViewer
 
 
-api=NinjaAPI()
+api=NinjaAPI(
+    docs=ScalarViewer(),
+)
+
 
 @api.get("/ping", response=PingResponseSchema)
 def ping(request: HttpRequest) -> PingResponseSchema:
     return PingResponseSchema(result=True)
+
 
 api.add_router('v1/', v1_router)
 
